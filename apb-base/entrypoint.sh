ae936fb161072d86d7a8316d13271835b6a0332b
Fabian von Feilitzsch <fabianvf@users.noreply.github.com>
2017-04-26 18:08:23 -0400

    When an unimplemented action is passed in, echo that it's not implemented but don't fail the build. This is basically a no-op (#8)

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-ANSIBLEAPP_ACTION=$1
+ACTION=$1
 shift
 playbooks=/opt/apb/actions
 
 oc-login.sh
 
-if [[ "$ANSIBLEAPP_ACTION" == "provision" ]]; then
-  ansible-playbook $playbooks/provision.yaml $@
-elif [[ "$ANSIBLEAPP_ACTION" == "deprovision" ]]; then
-  ansible-playbook $playbooks/deprovision.yaml $@
-elif [[ "$ANSIBLEAPP_ACTION" == "bind" ]]; then
-  ansible-playbook $playbooks/bind.yaml $@
-elif [[ "$ANSIBLEAPP_ACTION" == "unbind" ]]; then
-  echo "UNBIND NOT IMPLEMENTED" # TODO
+if [[ -e "$playbooks/$ACTION.yaml" ]]; then
+  ansible-playbook $playbooks/$ACTION.yaml $@
+elif [[ -e "$playbooks/$ACTION.yml" ]]; then
+  ansible-playbook $playbooks/$ACTION.yml $@
+else
+  echo "'$ACTION' NOT IMPLEMENTED" # TODO
 fi
