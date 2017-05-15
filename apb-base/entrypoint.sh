a2d50f8293f0024908ff42dccfca761f79d7443c
Joseph Callen <jcpowermac@gmail.com>
2017-05-15 11:04:11 -0400

    OpenShift arbitrary uid support for apb-base (#18)
    This PR adds the ability to run apb-base containers under OpenShift
    using the restricted scc and arbitrary uid support. These changes were based on previous work from:
    
    - https://github.com/RHsyseng/container-rhel-examples/tree/master/starter-arbitrary-uid
    - https://github.com/fusor/ansibleapp-library/pull/42
    
    I used this [repository](https://github.com/jcpowermac/apbtest) to test with.  Very basic to confirm that ansible will run correctly.
    
    [![before changes](https://asciinema.org/a/120161.png)](https://asciinema.org/a/120161)
    
    [![with changes](https://asciinema.org/a/120179.png)](https://asciinema.org/a/120179)

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ACTION=$1
+USER_ID=$(id -u)
 shift
 playbooks=/opt/apb/actions
 
+if [ ${USER_UID} != ${USER_ID} ]; then
+sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
+fi
 oc-login.sh
 
 if [[ -e "$playbooks/$ACTION.yaml" ]]; then
