4509fe31d169531307232beb9833733da3c0c3a1
Ryan Hallisey <rhallise@redhat.com>
2017-05-24 14:22:47 -0400

    Rework the Broker Bind Output
    - Script for the broker to use when it execs into the container
    - Script that will keep the apb running until the broker scoops
      up the bind credentials

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -4,15 +4,22 @@ USER_ID=$(id -u)
 shift
 playbooks=/opt/apb/actions
 
+set -x
+
 if [ ${USER_UID} != ${USER_ID} ]; then
   sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
 fi
 oc-login.sh
 
-if [[ -e "$playbooks/$ACTION.yaml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yaml "$@"
-elif [[ -e "$playbooks/$ACTION.yml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yml "$@"
+touch /etc/ansible-service-broker/bind-creds
+if [[ -e "$playbooks/$ACTION.yaml" || -e "$playbooks/$ACTION.yml" ]]; then
+  ansible-playbook $playbooks/$ACTION.yml "$@" || ansible-playbook $playbooks/$ACTION.yaml "$@"
+
+  # bind-init keeps the container running until the broker gathers the bind
+  # credentials by exec'ing into the container.
+  if [[ "${ACTION}" == "bind" || "${ACTION}" == "provision" ]]; then
+      bind-init
+  fi
 else
   echo "'$ACTION' NOT IMPLEMENTED" # TODO
 fi
