544feb5c099d8685c3cee2d216dbed56efdcc41f
Ryan Hallisey <rhallise@redhat.com>
2017-06-29 14:56:50 -0400

    Fix logic to support provision and bind
    This patch was written when only bind was returning
    credentials. Reconfigure this to support both provision and bind.

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -3,6 +3,7 @@ ACTION=$1
 USER_ID=$(id -u)
 shift
 playbooks=/opt/apb/actions
+CREDS="/etc/apb/bind-creds"
 
 set -x
 
@@ -11,14 +12,17 @@ if [ ${USER_UID} != ${USER_ID} ]; then
 fi
 oc-login.sh
 
-touch /etc/apb/bind-creds
 if [[ -e "$playbooks/$ACTION.yaml" || -e "$playbooks/$ACTION.yml" ]]; then
   ansible-playbook $playbooks/$ACTION.yml "$@" || ansible-playbook $playbooks/$ACTION.yaml "$@"
 
+  # If we are provisioning an APB, but it's not bindable then the bind-creds
+  # will never be created. Therefore, if bind-creds exists, we are running
+  # either provision or bind and the APB is bindable.
+  #
   # bind-init keeps the container running until the broker gathers the bind
   # credentials by exec'ing into the container.
-  if [[ "${ACTION}" == "bind" || "${ACTION}" == "provision" ]]; then
-      bind-init
+  if [ -f $CREDS ]; then
+     bind-init
   fi
 else
   echo "'$ACTION' NOT IMPLEMENTED" # TODO
