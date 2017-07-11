1b243f0fbd6894eca3ed7bc8141a0a1db6775daf
Derek Whatley <derekwhatley@gmail.com>
2017-07-11 14:00:24 -0400

    Modify entrypoint.sh in apb-base to properly handle spaces/special chars.

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -10,9 +10,9 @@ fi
 oc-login.sh
 
 if [[ -e "$playbooks/$ACTION.yaml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yaml $@
+  ansible-playbook $playbooks/$ACTION.yaml "$@"
 elif [[ -e "$playbooks/$ACTION.yml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yml $@
+  ansible-playbook $playbooks/$ACTION.yml "$@"
 else
   echo "'$ACTION' NOT IMPLEMENTED" # TODO
 fi
