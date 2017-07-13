e480a05dd8d85801d13574ae95b64a0b6a2e3aa6
Ryan Hallisey <rhallise@redhat.com>
2017-07-13 16:32:46 -0400

    Broker bind output (#25)
    * Rework the Broker Bind Output
    
    - Script for the broker to use when it execs into the container
    - Script that will keep the apb running until the broker scoops
      up the bind credentials
    
    * Update the existing APBs with bind to use the new bind method
    
    * Post rebase additions
    
    * Change ansible-service-broker to apb
    
    * Change bind creds directory from ansible-service-broker to apb in the playbooks
    
    * Fix logic to support provision and bind
    
    This patch was written when only bind was returning
    credentials. Reconfigure this to support both provision and bind.
    
    * Add a timeout to bind-init
    
    * Move all retry logic to the broker
    
    Only execute a one time command and exit so we keep
    the retry logic in one place.

--- apb-base/entrypoint.sh
+++ apb-base/entrypoint.sh
@@ -3,16 +3,27 @@ ACTION=$1
 USER_ID=$(id -u)
 shift
 playbooks=/opt/apb/actions
+CREDS="/etc/apb/bind-creds"
+
+set -x
 
 if [ ${USER_UID} != ${USER_ID} ]; then
   sed "s@${USER_NAME}:x:\${USER_ID}:@${USER_NAME}:x:${USER_ID}:@g" ${BASE_DIR}/etc/passwd.template > /etc/passwd
 fi
 oc-login.sh
 
-if [[ -e "$playbooks/$ACTION.yaml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yaml "$@"
-elif [[ -e "$playbooks/$ACTION.yml" ]]; then
-  ansible-playbook $playbooks/$ACTION.yml "$@"
+if [[ -e "$playbooks/$ACTION.yaml" || -e "$playbooks/$ACTION.yml" ]]; then
+  ansible-playbook $playbooks/$ACTION.yml "$@" || ansible-playbook $playbooks/$ACTION.yaml "$@"
+
+  # If we are provisioning an APB, but it's not bindable then the bind-creds
+  # will never be created. Therefore, if bind-creds exists, we are running
+  # either provision or bind and the APB is bindable.
+  #
+  # bind-init keeps the container running until the broker gathers the bind
+  # credentials by exec'ing into the container.
+  if [ -f $CREDS ]; then
+     bind-init
+  fi
 else
   echo "'$ACTION' NOT IMPLEMENTED" # TODO
 fi
